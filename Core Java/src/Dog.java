9ac41f3c931932b44e27b68f991c6eaf3dff7cda
mirfaizanali <mir_faizan_ali@outlook.com>
2025-08-20 20:32:04 +0530

    Added a class and some functionality

---
+++ Core Java/src/Dog.java
@@ -0,0 +1,10 @@
+public class Dog extends Animal {
+
+    private String coloor;
+    public String getColoor() {
+        return coloor;
+    }
+    public void setColoor(String coloor) {
+        this.coloor = coloor;
+    }
+}
--- Core Java/src/Inheritance.java
+++ Core Java/src/Inheritance.java
@@ -3,7 +3,14 @@ public class Inheritance {
     public static void main(String[] args) {
         Animal animal = new Animal();
 
-        animal.setName("Tommy");
+        Dog dog = new Dog();
+
+        dog.setColoor("Green");
+        dog.setName("Tom");
+        animal.setName("tommy");
+
         System.out.println(animal.getName());
+        System.out.println(dog.getName());
+        System.out.println(dog.getColoor());
     }
 }
