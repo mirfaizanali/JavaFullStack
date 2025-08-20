c23519bb86955702f5b942b31ac5c5d323bfcb4e
mirfaizanali <mir_faizan_ali@outlook.com>
2025-08-20 20:28:18 +0530

    Initial Test commit

---
+++ Core Java/src/Inheritance.java
@@ -0,0 +1,9 @@
+public class Inheritance {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+
+        animal.setName("Tommy");
+        System.out.println(animal.getName());
+    }
+}
